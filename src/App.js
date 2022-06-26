5bffaef4d59a0c2ce428c6404885e6e0c640cc9b
Isaac Lopez <isaaclopez@Isaacs-MacBook-Air.local>
2022-06-25 22:10:44 -0500

    feat: some pages have been added. style: the style has been added for the main page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import Card from './Cards';
-import Navbar from './Cards'
-import { getAllPokemon } from './pokemon';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import { getPokemon, getAllPokemon } from './services/pokemon';
 import './App.css';
 
-
 function App() {
-    const [pokemonData, setPokemonData] = useState(null);
-    const [nextUrl, setNextUrl] = useState('');
-    const [prevUrl, setPrevUrl] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
 
-    useEffect(() => {
-      async function fetchData() {
-        let response = await getAllPokemon(initialUrl);
-        console.log(response);
-        setNextUrl (response.next);
-        setPrevUrl (response.previous);
-        await loadingPokemon(response.results);
-        setIsLoading(false);
-      }
-      fetchData();
-    }, []);
-
-    const loadingPokemon = async data => {
-      let _pokemonData = await Promise.all(data.map(async pokemon => {
-        let pokemonRecord = await getPokemon(pokemon.url); 
-        return pokemonRecord
-      }));
-
-      setPokemonData(_pokemonData);
-
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialURL)
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadPokemon(response.results);
+      setLoading(false);
     }
-    return <div> {isLoading ? <h1>Loading...</h1> : 
-          <h1>Data fetched successfully</h1>}</div>;
+    fetchData();
+  }, [])
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon)
+      return pokemonRecord
+    }))
+    setPokemonData(_pokemonData);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        {loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> : (
+          <>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+            <div className="grid-container">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
